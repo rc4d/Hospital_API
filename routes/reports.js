8d85daa6dabebf9bc7464f3e17a784835c771716
Robin <guptarobin466@gmail.com>
2021-03-20 21:47:44 +0530

    router updated

--- routes/reports.js
+++ routes/reports.js
@@ -5,5 +5,5 @@ const passport = require('passport');
 const report_api = require("../controllers/reportsapi");
 
 router.get('/:status',passport.authenticate('jwt',{session: false}),report_api.reports);
-router.get('/', report_api);
+
 module.exports = router;
